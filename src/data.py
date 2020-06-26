7ccaa67b286568465ba54e3a84010f20e032118c
Philip Oesterlin <philip.oesterlin@gmail.com>
2020-06-26 16:59:36 +0200

    add train time output

--- src/data.py
+++ src/data.py
@@ -15,7 +15,7 @@ class Loader():
         trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform) 
         trainloader = torch.utils.data.DataLoader(trainset, batch_size=batches, shuffle=True, num_workers=2)
         testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-        testloader = torch.utils.data.DataLoader(testset, batch_size=3000, shuffle=False, num_workers=1)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=10000, shuffle=False, num_workers=1)
         self.classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
         self.trainloader = trainloader
         self.testloader = testloader
